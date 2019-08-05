dd76eb6093554ba31dc2862a8ea7a646267a4abb
pmullerCBK <pmuller@codedbykids.com>
2019-08-04 21:12:08 -0400

    Various css and text changes

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 jQuery(document).ready(($) => {
 	$('.navigation').load("../reusable/navigation.html")
+	$('.footer').load("../reusable/footer.html")
+	$('.contact').load("../reusable/contact.html")
 })
 jQuery(document).ready(($) => {
-	$('.footer').load("../reusable/footer.html")
 })
