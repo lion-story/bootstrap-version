6772af051178c68c11f87259c223e226cf3f52bc
pmullerCBK <pmuller@codedbykids.com>
2019-08-04 21:47:56 -0400

    Attempt 2 to fix no jquery on pagesw

--- js/index.js
+++ js/index.js
@@ -5,4 +5,7 @@ jQuery(document).ready(($) => {
 	$('.contact').load("../reusable/contact.html")
 })
 jQuery(document).ready(($) => {
+	$('.navigation').load("reusable/navigation.html")
+	$('.footer').load("reusable/footer.html")
+	$('.contact').load("reusable/contact.html")
 })
